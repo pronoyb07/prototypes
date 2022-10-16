2515a8df9c5c7f99bbd04042e8f266d707c962b3
Pronoy Bhattacharya <peonoyb07@gmail.com>
2022-10-16 21:20:10 +0530

    fixed colours of the website

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const checkbox = document.getElementById("checkbox")
-checkbox.addEventListener("change", () => {
-    document.body.classList.toggle("dark")
+// const checkbox = document.getElementById("checkbox")
+// checkbox.addEventListener("change", () => {
+//     document.body.classList.toggle("dark")
+// })
+
+let toggler = document.querySelector(".toggler");
+let fullscreen = document.querySelector(".fullscreen");
+
+toggler.addEventListener('click', () => {
+    fullscreen.classList.toggle("light");
 })
