358dac25a8fb80447b6376a4bca666a50fd8c1ce
Pronoy Bhattacharya <peonoyb07@gmail.com>
2022-11-18 19:25:41 +0530

    React - setState

--- hello/src/App.js
+++ hello/src/App.js
@@ -5,12 +5,14 @@ import {
 } from 'react';
 import Hello from './components/hello';
 import Message from './components/message';
+import Counter from './components/counter';
 
 class App extends Component {
   render() {
     return ( 
-    <div className = 'App' >
-      <Message />
+    <div className = "App" >
+      <Counter />
+      
       </div>
     )
   }
