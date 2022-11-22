2ba076dd807c2009e1207f74d523a830fce97a81
Pronoy Bhattacharya <peonoyb07@gmail.com>
2022-11-22 12:22:26 +0530

    Binding Event Handlers

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import {
-  Component
-} from 'react';
+import {Component} from 'react';
 import Hello from './components/hello';
 import Message from './components/message';
 import Counter from './components/counter';
 import FunctionClick from './components/functionClick';
+import EventBind from './components/EventBind';
+
+
 
 class App extends Component {
   render() {
     return ( 
     <div className = "App" >
-      <FunctionClick />
+      <EventBind/>
       
       </div>
     )
---
+++ hello/src/components/EventBind.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class EventBind extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         message: 'Hello'
+      }
+
+    //   this.clickHandler = this.clickHandler.bind(this)
+    }
+
+    // clickHandler() {
+    //     this.setState({
+    //         message: 'Goodbye!'
+    //     })
+    //     console.log(this)
+    // }
+
+    clickHandler = () => {
+        this.setState({
+            message: 'Goodbye!!!!'
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div>{this.state.message}</div>
+
+                {/* <button onClick={this.clickHandler.bind(this)}>
+                    Click Me!!
+                </button> */}
+                {/* <button onClick={() => this.clickHandler()}>Click Me!!</button> */}
+                <button onClick={ this.clickHandler}>Click Me!!</button>
+                
+            </div>
+        )
+    }
+}
+
+export default EventBind
