983020f9a355cbd7fdba37590f9894c6dbeb96e3
Pronoy Bhattacharya <peonoyb07@gmail.com>
2022-11-22 18:04:31 +0530

    Form Handling

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import {Component} from 'react';
-import Hello from './components/hello';
-import Message from './components/message';
-import Counter from './components/counter';
-import FunctionClick from './components/functionClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-
-
+import Forms from './components/forms';
 
 class App extends Component {
   render() {
     return ( 
     <div className = "App" >
-      <Stylesheet primary={true}/>
-      
+      <Forms/>
       </div>
     )
   }
