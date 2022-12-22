59e7448eec17c267b1f013c4984e552bb3a1a334
Pronoy Bhattacharya <peonoyb07@gmail.com>
2022-12-22 17:24:26 +0530

    environment setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PortalDemo from './components/PortalDemo';
+
+const App = () => {
+  return React.createElement(
+    'h1', 
+    {}, 
+    'Hello World 👋'
+  );
+};
+
+
+export default App;
